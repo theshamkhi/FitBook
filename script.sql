41622fb5092ffac22ea03c36f7632e2a1cc3091d
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2024-12-11 22:33:20 +0100

    SQL scripts for database manipulation

--- script.sql
+++ script.sql
@@ -24,10 +24,11 @@ CREATE TABLE reservations (
     FOREIGN KEY (ActiviteID) REFERENCES Activites(ActiviteID)
 );
 
--- Inserting for testing
-INSERT INTO membres (Nom, Prenom, Telephone, Email) VALUES
-('CHAMKHI', 'Mohammed', '212-636-253939', 'theshamkhi1@gmail.com');
+-- Example of inserting --
 
+INSERT INTO membres (Nom, Prenom, Telephone, Email) VALUES
+('CHAMKHI', 'Mohammed', '212-636-253939', 'theshamkhi1@gmail.com'),
+('MARSLI', 'Younes', '123-456-789', 'marsli.younes@gmail.com');
 
 INSERT INTO activites (Nom, Description) VALUES
 ('Yoga', 'A relaxing activity focusing on stretching and mindfulness.'),
@@ -35,6 +36,28 @@ INSERT INTO activites (Nom, Description) VALUES
 ('Pilates', 'A low-impact exercise for flexibility, strength, and posture.'),
 ('CrossFit', 'A high-intensity workout combining strength and cardio exercises.');
 
-
 INSERT INTO reservations (MembreID, ActiviteID, ReservationDate) VALUES 
-(1, 1, '2024-12-15 09:00:00');
+(1, 1, '2024-12-15 09:00:00'),
+(2, 2, '2024-12-16 10:30:00');
+
+
+-- Example of modifying --
+
+UPDATE activites 
+SET Description = 'An intensive cycling class designed to improve stamina.'
+WHERE ActiviteID = 2;
+
+UPDATE reservations 
+SET ReservationDate = '2024-12-17 11:00:00'
+WHERE ReservationID = 2;
+
+-- Example of deleting --
+
+DELETE FROM membres 
+WHERE MembreID = 2;
+
+DELETE FROM activites 
+WHERE ActiviteID = 4;
+
+DELETE FROM reservations 
+WHERE ReservationID = 1;
